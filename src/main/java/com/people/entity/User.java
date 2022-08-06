2c0211100545a0bd9901084a441f27d09f5fa142
“chengpopeye” <1084581668@qq.com>
2022-08-06 15:59:33 +0800

    mp学习

---
+++ src/main/java/com/people/entity/User.java
@@ -0,0 +1,8 @@
+package com.people.entity;/**
+ * @description: TODO
+ * @author      chengdali
+ * @datetime    2022/8/6
+ * @version:    1.0
+ */
+public class User {
+}
---
+++ src/main/java/com/people/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.people.mapper;/**
+ * @description: TODO
+ * @author      chengdali
+ * @datetime    2022/8/6
+ * @version:    1.0
+ */
+
+/**
+ * UserMapper  
+ * @author chengdali 
+ * @version V1.0
+ * @date 2022/08/06
+**/
+public interface UserMapper {
+}
+
